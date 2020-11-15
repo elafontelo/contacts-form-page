2e0d3d92c7db2008dbe089ae1bfb28643250c22c
Ysraela Fontelo <ysraela.f@kinpo.com.ph>
2020-11-16 00:15:37 +0800

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using ContactsApp.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ContactForm(Contacts input)
+        {
+            Contacts contact = new Contacts();
+
+            contact.firstName = input.firstName;
+            contact.lastName = input.lastName;
+            contact.address = input.address;
+            contact.state = input.state;
+            contact.country = input.country;
+            contact.zipCode = input.zipCode;
+            contact.phoneNumber = input.phoneNumber;
+            contact.notes = input.notes;
+
+            using (MySqlConnection connect = new MySqlConnection("Data Source=localhost; port=3306; Database=testdb; UserId=root; password=123456"))
+            {
+                connect.Open();
+                string insert = "Insert into contacts(firstName, lastName, address, state, country, zipCode, phoneNumber, notes) values('" + input.firstName + "', '" + input.lastName + "', '" + input.address + "', '" + input.state + "', '" + input.country + "', '" + input.zipCode + "', '" + input.phoneNumber + "', '" + input.notes + "')";
+                MySqlCommand insertCmd = connect.CreateCommand();
+                insertCmd.CommandType = CommandType.Text;
+                insertCmd.CommandText = insert;
+                insertCmd.ExecuteNonQuery();
+                connect.Close();
+            }
+
+            return RedirectToAction("ConfirmPage");
+        }
+
+        public ActionResult ConfirmPage()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Contacts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContactsApp.Models
+{
+    public class Contacts
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public int? zipCode { get; set; }
+        public string phoneNumber { get; set; }
+        public string notes { get; set; }
+
+    }
+}
